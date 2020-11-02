a8a74901562251a973858d3936ab0f1f2f451aaa
Yaineya <evgeniydorofeev@MacBook-Air-ll.local>
2020-11-02 20:58:50 +0300

    car started

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,31 +1,48 @@
 "use strict";
 
-// по фарам
-var lamp = document.querySelector(".lamp");
-var elementLamps = document.querySelector(".lamp_off"); // по аварийке
+window.onload = function () {
+  // по фарам
+  // let lamp = document.querySelector(".lamp");
+  var lamp_btn = $("[data-js=hoodLamps]");
+  var elementLamps = document.querySelectorAll(".hood .lamp"); // по аварийке
 
-var btn_emergency = document.querySelector(".emergency");
-var turn = document.querySelector(".turn_signal");
-lamp.addEventListener("click", head_Lamp);
-btn_emergency.addEventListener("click", emerg);
+  var btn_emergency = document.querySelector(".emergency");
+  var turn = document.querySelector(".turn_signal"); // lamp.addEventListener("click",head_Lamp); = то что ниже
 
-function head_Lamp() {
-  if (elementLamps.className = "lamp_off") {
-    elementLamps.className = "lamp_on";
-  } else {
-    elementLamps.className = "lamp_off";
+  lamp_btn.on("click", head_Lamp);
+  btn_emergency.addEventListener("click", emerg);
+
+  function head_Lamp() {
+    elementLamps.forEach(function (lamp) {
+      // if (lamp.className.includes("off")) {
+      //     lamp.className = "on"
+      // } else {
+      //     lamp.className = "off"
+      // }
+      var classList = lamp.classList;
+      var isOff = classList.contains('off');
+
+      if (isOff) {
+        classList.add('on');
+        classList.remove('off');
+      } else {
+        classList.remove('on');
+        classList.add('off');
+      } // l.className = l.className === "lamp_off" 
+      //     ? "lamp_on"
+      //     : "lamp_off"
+
+    });
   }
-}
 
-function emerg() {}
+  function emerg() {}
 
-; // window.onload = function() {
-//     document.addEventListener('click', function(e) {
-//         var classList = e.target.classList;
-//         var isLamp = classList.contains('lamp');
-//         if (!isLamp) return;
-//         var isOn = classList.contains('on');
-//         if (isOn) classList.remove('on');
-//         else classList.add('on');
-//     });
-// };
+  ; //     document.addEventListener('click', function(e) {
+  //         var classList = e.target.classList;
+  //         var isLamp = classList.contains('lamp');
+  //         if (!isLamp) return;
+  //         var isOn = classList.contains('on');
+  //         if (isOn) classList.remove('on');
+  //         else classList.add('on');
+  //     });
+};
--- script.js
+++ script.js
@@ -1,36 +1,59 @@
-// по фарам
-let lamp = document.querySelector(".lamp");
-let elementLamps = document.querySelector(".lamp_off");
-
-// по аварийке
-let btn_emergency= document.querySelector(".emergency")
-let turn = document.querySelector(".turn_signal")
-
-lamp.addEventListener("click",head_Lamp);
-btn_emergency.addEventListener("click",emerg);
+window.onload = function() {
 
 
-function head_Lamp() {
-    if (elementLamps.className = "lamp_off") {
-        elementLamps.className = "lamp_on"
-    } else {
-        elementLamps.className = "lamp_off"
+    // по фарам
+    // let lamp = document.querySelector(".lamp");
+    let lamp_btn = $("[data-js=hoodLamps]");
+    let elementLamps = document.querySelectorAll(".hood .lamp");
+
+    // по аварийке
+    let btn_emergency= document.querySelector(".emergency")
+    let turn = document.querySelector(".turn_signal")
+
+    // lamp.addEventListener("click",head_Lamp); = то что ниже
+    lamp_btn.on("click",head_Lamp);
+    btn_emergency.addEventListener("click",emerg);
+
+
+    function head_Lamp() {
+        elementLamps.forEach((lamp)=>{
+            // if (lamp.className.includes("off")) {
+            //     lamp.className = "on"
+            // } else {
+            //     lamp.className = "off"
+            // }
+            var classList = lamp.classList;
+
+            var isOff = classList.contains('off');
+            if (isOff) {
+                classList.add('on');
+                classList.remove('off');
+            } else{
+                classList.remove('on');
+                classList.add('off');
+            }
+
+            // l.className = l.className === "lamp_off" 
+            //     ? "lamp_on"
+            //     : "lamp_off"
+        })
+
+
+        
     }
-}
 
-function emerg(){};
-
+    function emerg(){};
 
 
 
-// window.onload = function() {
-//     document.addEventListener('click', function(e) {
-//         var classList = e.target.classList;
-//         var isLamp = classList.contains('lamp');
-//         if (!isLamp) return;
 
-//         var isOn = classList.contains('on');
-//         if (isOn) classList.remove('on');
-//         else classList.add('on');
-//     });
-// };
+    //     document.addEventListener('click', function(e) {
+    //         var classList = e.target.classList;
+    //         var isLamp = classList.contains('lamp');
+    //         if (!isLamp) return;
+
+    //         var isOn = classList.contains('on');
+    //         if (isOn) classList.remove('on');
+    //         else classList.add('on');
+    //     });
+};
